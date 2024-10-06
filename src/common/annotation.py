c0a2afca2a2df9d9187dbdeb9555ca119d9863a9
iliodium <95029175+iliodium@users.noreply.github.com>
2024-10-06 18:28:20 +0300

    убрал лишние комментарии

--- src/common/annotation.py
+++ src/common/annotation.py
@@ -1,12 +1,12 @@
 from typing import (Literal,
                     Tuple,
-                    Union,)
+                    Union, )
 
 from sqlalchemy.engine.base import Engine
 
 from src.common.constants import (alpha_standards,
                                   ks10,
-                                  wind_regions,)
+                                  wind_regions, )
 from src.common.FaceType import FaceType
 
 type AlphaType = Literal[4, 6]
@@ -57,48 +57,3 @@ type WindRegionsType = Literal[*wind_regions]
 def check_type_engine(engine):
     if not isinstance(engine, Engine):
         raise Exception("engine must be an instance of sqlalchemy.engine.Engine")
-
-
-'''
-from src.common.annotation import AngleType
-
-
-        check_type_engine(_engine)
-
-        experiment_id: ExperimentIdType,
-        alpha: AlphaType,
-        _engine,
-        *,
-        angle: AngleType = None,
-        face_number: FaceType = None,
-        position_x: PositionXType = None,
-        position_y: PositionYType = None
-
-@validate_call
-'''
-'''
-__FilterPressureCoefficients src/submodules/databasetoolkit/isolated.py
-типы переписал на свои, но не бросает ошибку, если что то не так при создании тк класс
-для чего он нужен ?
-
-get_view_permutation_data src/submodules/utils/permutations.py
-нужно ли ограничение что угол 0<=angle<360 ?
-тк если пользователь вводит >=360, то мы угол % 360 всегда
-тк 0 == 360 == 720 и тд
-можно так то это ограничение глобально задать 
-type AngleType = int
-Annotated[AngleType, Field(ge=0, lt=360)]
-
-calculate_cmz src/submodules/science/integration.py
-не работает валидация на _pressure_coefficients: np.ndarray,
-
-
-
-
-
-
-
-
-
-
-'''
