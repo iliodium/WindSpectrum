274f5abba4702b6bf79ef63e00f827e2c7018906
iliodium <95029175+iliodium@users.noreply.github.com>
2024-10-07 14:11:16 +0300

    работа над ci cd

--- .github/workflows/import_linter/import_linter.bat
+++
@@ -1,8 +0,0 @@
-echo off
-set folder=%1
-set command-setting-path=%2
-set setting-path=%3
-
-echo Script is running in directory: %cd%
-
-isort src/ --settings-path .github/workflows/import_linter/.isort.cfg
---
+++ .github/workflows/import_linter/import_linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+folder=$1
+command_setting_path=$2
+setting_path=$3
+
+
+isort "folder" "command_setting_path" "setting_path"
